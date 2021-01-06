e97a3018f067e9f6eb306a8b0753cc7bd226778c
Hari Sekhon <harisekhon@gmail.com>
2021-01-06 17:12:01 +0000

    updated Dockerfiles.kt

--- kotlin/GitHub/buildTypes/Dockerfiles.kt
+++ kotlin/GitHub/buildTypes/Dockerfiles.kt
@@ -6,7 +6,7 @@ object Dockerfiles : BuildType({
     templates(HariSekhon_Make)
     uuid = "4a56dded-b4d2-460b-9e8b-7fea3036b19b"
     name = "Dockerfiles"
-    description = "50+ DockerHub public images for Docker & Kubernetes - Hadoop, Kafka, ZooKeeper, HBase, Cassandra, Solr, SolrCloud, Presto, Apache Drill, Nifi, Spark, Consul, Riak, and DevOps tools built various distros: Alpine, CentOS, Debian, Fedora, Ubuntu"
+    description = "50+ DockerHub public images for Docker & Kubernetes - Hadoop, Kafka, ZooKeeper, HBase, Cassandra, Solr, SolrCloud, Presto, Apache Drill, Nifi, Spark, Consul, Riak, TeamCity and DevOps tools built on the major Linux distros: Alpine, CentOS, Debian, Fedora, Ubuntu"
 
     vcs {
         root(GitHub.vcsRoots.Dockerfiles_1)
