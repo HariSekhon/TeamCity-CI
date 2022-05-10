364f9b34c928224468ca720756706c8813606709
Hari Sekhon <harisekhon@gmail.com>
2022-05-10 11:57:45 +0100

    updated URLs

--- kotlin/GitHub/vcsRoots/HAProxyConfigs_1.kt
+++ kotlin/GitHub/vcsRoots/HAProxyConfigs_1.kt
@@ -6,7 +6,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 object HAProxyConfigs_1 : GitVcsRoot({
     uuid = "52539268-d78d-4d47-9271-d3b546e34f2a"
     id("HAProxyConfigs")
-    name = "https://github.com/HariSekhon/HAProxy-Configs"
-    url = "https://github.com/HariSekhon/HAProxy-Configs"
+    name = "https://github.com/HariSekhon/HAProxy-configs"
+    url = "https://github.com/HariSekhon/HAProxy-configs"
     branch = "refs/heads/master"
 })
